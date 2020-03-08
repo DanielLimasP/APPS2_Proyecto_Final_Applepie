e637d8c012630bd8357df544065c6339c74b3445
Daniel Limas <48876636+DanielLimasP@users.noreply.github.com>
2020-03-07 22:17:41 -0700

    Ultimo commit antes de hacer merge entre app de login con mongo y app de login con google
    Que Dios nos ampare...

---
+++ app/src/main/java/com/example/proyecto_applepie/Retrofit/IMyService.java
@@ -0,0 +1,18 @@
+package com.example.proyecto_applepie.Retrofit;
+
+import io.reactivex.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface IMyService {
+    @POST("signup")
+    @FormUrlEncoded
+    Observable<String> signupUser(@Field("email") String email,
+                                  @Field("name") String name,
+                                  @Field("password") String password);
+    @POST("login")
+    @FormUrlEncoded
+    Observable<String> loginUser(@Field("email") String email,
+                                 @Field("password") String password);
+}
